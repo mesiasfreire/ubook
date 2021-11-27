542c190397919b5f439937b3b65ac884c327a7f0
Messias IBlue <p1210160@brasilprev.com.br>
2021-11-27 18:09:57 -0300

    feat: create a new Home component

---
+++ src/views/Home/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <Agenda />
+  <section
+    class="flex justify-center items-center flex-col mx-auto gap-4 my-20"
+  >
+    <img src="@/assets/icons/ic-book.svg" alt="Nenhum contato foi criado" />
+    <p>Nenhum contato foi criado ainda</p>
+    <button
+      class="rounded-full bg-yellowGreen-50 py-2 w-44 mx-4 shadow-md text-tomato-25 font-medium font- flex items-center justify-evenly"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      Criar contato
+    </button>
+  </section>
+</template>
+
+<script>
+import { reactive } from "vue";
+import Agenda from "@/components/Agenda";
+
+export default {
+  name: "Home",
+  components: {
+    Agenda,
+  },
+  setup() {
+    const agenda = reactive({
+      count: 0,
+    });
+
+    return {
+      agenda,
+    };
+  },
+};
+</script>
