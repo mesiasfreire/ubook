ac9ca19f71c47e11a334632fe67d8535a17f7cec
Messias IBlue <p1210160@brasilprev.com.br>
2021-11-27 18:14:37 -0300

    feat: create new template at App component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <router-view />
+  <section class="h-screen w-screen bg-main-grayligth p-4 overflow-hidden">
+    <main class="max-w-7xl mx-auto">
+      <Header />
+      <router-view />
+    </main>
+  </section>
 </template>
+<script>
+import Header from "@/components/Header";
+export default {
+  components: {
+    Header,
+  },
+  setup() {},
+};
+</script>
